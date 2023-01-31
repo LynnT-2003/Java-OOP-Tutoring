2b994926a448ec37e519804025e2aca86303da01
LynnT-2003 <lnyinyi22@gmail.com>
2023-01-31 13:56:16 +0700

    added Quiz_2 Question_3 from 2022

---
+++ Quiz2_q3_2022/Cylinder.java
@@ -0,0 +1,51 @@
+package Quiz2_q3_2022;
+
+public class Cylinder extends Circle {
+
+    // 1. Data members
+    private double height = 1.0;
+
+    // 2. Constructors
+    public Cylinder() {
+
+    }
+
+    public Cylinder(double radius) {
+        super(radius);
+    }
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public Cylinder(double radius, double height, String color) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    // 3. Methods
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.PI * getRadius() * getRadius() * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "radius=" + getRadius() +
+                ", height=" + height +
+                ", color=" + getColor() +
+                ", volume=" + getVolume() +
+                '}';
+    }
+}
